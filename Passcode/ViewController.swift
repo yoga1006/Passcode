53629a9a5577a89a6b531293ec06f8e0a1b70b55
Yoga <jc051533@gmail.com>
2022-06-21 23:53:54 +0800

    passcodeApp

--- Passcode/ViewController.swift
+++ Passcode/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var backgroundImageView: UIImageView!
     
+    
     @IBOutlet var numberButton: [UIButton]!
     
     @IBOutlet weak var passcode1ImageView: UIImageView!
@@ -20,6 +21,10 @@ class ViewController: UIViewController {
     
     var correctPassCode = "2022"
     var enterPassCode = ""
+    var count = 0
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,27 +37,49 @@ class ViewController: UIViewController {
     
     //檢查輸入的密碼是否正確，跳出提醒顯示
     func checkPassCode(){
+        
+        if count == 4{
         if enterPassCode == correctPassCode{
             let controller = UIAlertController(title: "正確！", message: "歡迎使用！", preferredStyle: .alert)
-            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default){_ in self.reset()}
             controller.addAction(okAction)
             present(controller,animated: true,completion: nil)
-        }else{
+        }else {
             let controller = UIAlertController(title: "錯誤！", message: "請重新輸入！", preferredStyle: .alert)
-            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default){_ in self.reset()}
             controller.addAction(okAction)
             present(controller,animated: true,completion: nil)
         }
+        }
+        
+        
+    }
+    
+    func reset(){
+        passcode1ImageView.isHighlighted = false
+        passcode2ImageView.isHighlighted = false
+        passcode3ImageView.isHighlighted = false
+        passcode4ImageView.isHighlighted = false
+        count = 0
+        enterPassCode = ""
     }
 
     //輸入密碼時imageview改變成實心
     func imageChange(){
-        switch enterPassCode.count{
+        switch count{
+        case 0 :
+            passcode1ImageView.isHighlighted = false
+            passcode2ImageView.isHighlighted = false
+            passcode3ImageView.isHighlighted = false
+            passcode4ImageView.isHighlighted = false
+            
         case 1:
             passcode1ImageView.isHighlighted = true
             passcode2ImageView.isHighlighted = false
             passcode3ImageView.isHighlighted = false
             passcode4ImageView.isHighlighted = false
+            
+            
         case 2:
             passcode1ImageView.isHighlighted = true
             passcode2ImageView.isHighlighted = true
@@ -68,28 +95,48 @@ class ViewController: UIViewController {
             passcode2ImageView.isHighlighted = true
             passcode3ImageView.isHighlighted = true
             passcode4ImageView.isHighlighted = true
-            checkPassCode()
-        default:
-            passcode1ImageView.isHighlighted = false
-            passcode2ImageView.isHighlighted = false
-            passcode3ImageView.isHighlighted = false
-            passcode4ImageView.isHighlighted = false
-
+            
+        default :
+            break
+            
         }
     }
     
-    
-    
     @IBAction func check(_ sender: UIButton) {
-
-        if enterPassCode.count != 4{
-            if let inputNumber = sender.currentTitle{
-                //字串相加
-                enterPassCode.append(inputNumber)
-            }
+    
+        //每按一個數字count+1
+        count += 1
+        //當count小於等於四碼時
+        if count <= 4{
+            //enterPassCode加入輸入的數字，因sender.tag為Int，需轉為enterPassCode的型別String
+            enterPassCode += "\(sender.tag)"
+     
         }
+      
+        //同步改變imageView
         imageChange()
-        print(enterPassCode)
-}
+        //同步檢查密碼
+        checkPassCode()
+    }
+    
+    
+    @IBAction func restart(_ sender: UIButton) {
+        reset()
+    }
+    
+    //清除密碼一位數
+    @IBAction func deletnum(_ sender: UIButton) {
+        //當密碼數不等於0位時
+        if count != 0{
+     
+            enterPassCode.removeLast()
+            //每減一位數，count也需-1
+            count -= 1
+           
+        }
+        //同步改變imageview
+        imageChange()
+    }
+    
     
 }
